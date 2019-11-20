7786869f762c69ce2189469e7049113e7c9b46a6
Brian Redmond <bredmond@salesforce.com>
2019-11-20 15:43:46 -0500

    Add license header

--- test/utils.js
+++ test/utils.js
@@ -1,4 +1,9 @@
-/* eslint-disable no-undef */
+/*
+ * Copyright (c) 2019, salesforce.com, inc.
+ * All rights reserved.
+ * SPDX-License-Identifier: BSD-3-Clause
+ * For full license text, see the LICENSE file in the repo root or https://opensource.org/licenses/BSD-3-Clause
+ */
 "use strict";
 const assert = require("chai").assert;
 const tmp = require("tmp");
