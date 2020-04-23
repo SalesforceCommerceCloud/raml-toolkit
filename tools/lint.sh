9ce889e118b415ffa911bbb7e3dd5ae7bdc22f3b
Kannan Alagar-Raj <kalagarraj@salesforce.com>
2020-04-23 10:27:56 -0400

    Linter tools

---
+++ tools/lint.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+packFilenamePattern="commerce-apps-raml-toolkit*.tgz"
+cd /linter && npm pack
+npmPackFiles=($packFilenamePattern) && linterPackFile=${npmPackFiles[0]}
+node /linter/tools/lint.js /linter/${linterPackFile}
+
+
