e5f05df023ec792239c019dee35366bb255b7904
Brian Redmond <bredmond@salesforce.com>
2020-04-29 16:01:41 -0400

    Update resource validation rule

--- test/mercury/resource-validation.test.ts
+++ test/mercury/resource-validation.test.ts
@@ -11,41 +11,38 @@ import {
   getHappySpec,
   conforms,
   breaksOnlyOneRule,
+  breaksTheseRules,
   renameKey,
   renderSpecAsFile
 } from "../utils.test";
 
 const PROFILE = "mercury";
+const NAME_VALIDATION =
+  "http://a.ml/vocabularies/data#resource-name-validation";
 
 describe("resource checking tests", () => {
   it("does not conform when resource is in capitals", async () => {
     const doc = getHappySpec();
     renameKey(doc, "/resource", "/RESOURCE");
     const result = await validateFile(renderSpecAsFile(doc), PROFILE);
-    breaksOnlyOneRule(
-      result,
-      "http://a.ml/vocabularies/data#resource-name-validation"
-    );
+    // This validates the resolved path of each resource so this causes a
+    // failure for this resource AND any child resources. We need to expect
+    // multiple failures
+    breaksTheseRules(result, [NAME_VALIDATION, NAME_VALIDATION]);
   });
 
   it("does not conform when resource starts with underscore", async () => {
     const doc = getHappySpec();
     renameKey(doc, "/resource", "/_RESOURCE");
     const result = await validateFile(renderSpecAsFile(doc), PROFILE);
-    breaksOnlyOneRule(
-      result,
-      "http://a.ml/vocabularies/data#resource-name-validation"
-    );
+    breaksTheseRules(result, [NAME_VALIDATION, NAME_VALIDATION]);
   });
 
   it("does not conform when resource ends with dash", async () => {
     const doc = getHappySpec();
     renameKey(doc, "/resource", "/resource-");
     const result = await validateFile(renderSpecAsFile(doc), PROFILE);
-    breaksOnlyOneRule(
-      result,
-      "http://a.ml/vocabularies/data#resource-name-validation"
-    );
+    breaksTheseRules(result, [NAME_VALIDATION, NAME_VALIDATION]);
   });
 
   it("conforms when resource contains numbers", async () => {
@@ -66,10 +63,35 @@ describe("resource checking tests", () => {
     const doc = getHappySpec();
     renameKey(doc, "/resource", "/this_resource");
     const result = await validateFile(renderSpecAsFile(doc), PROFILE);
-    breaksOnlyOneRule(
-      result,
-      "http://a.ml/vocabularies/data#resource-name-validation"
-    );
+    breaksTheseRules(result, [NAME_VALIDATION, NAME_VALIDATION]);
+  });
+
+  it("conforms when resource contains multiple literal parts", async () => {
+    const doc = getHappySpec();
+    renameKey(doc, "/resource", "/path/is/ok");
+    const result = await validateFile(renderSpecAsFile(doc), PROFILE);
+    conforms(result);
+  });
+
+  it("conforms when resource contains multiple literal parts and template", async () => {
+    const doc = getHappySpec();
+    renameKey(doc, "/resource", "/path/is/{ok}");
+    const result = await validateFile(renderSpecAsFile(doc), PROFILE);
+    conforms(result);
+  });
+
+  it("does not conform when resource starts with symbol", async () => {
+    const doc = getHappySpec();
+    renameKey(doc, "/resource", "/-path");
+    const result = await validateFile(renderSpecAsFile(doc), PROFILE);
+    breaksTheseRules(result, [NAME_VALIDATION, NAME_VALIDATION]);
+  });
+
+  it("does not conform when resource contains multiple literal parts and starts with symbol", async () => {
+    const doc = getHappySpec();
+    renameKey(doc, "/resource", "/-path/is/ok");
+    const result = await validateFile(renderSpecAsFile(doc), PROFILE);
+    breaksTheseRules(result, [NAME_VALIDATION, NAME_VALIDATION]);
   });
 
   it("conforms when resource contains hyphens", async () => {
@@ -79,7 +101,7 @@ describe("resource checking tests", () => {
     conforms(result);
   });
 
-  it("fails when template is in caps", async () => {
+  it("conforms when template is in caps", async () => {
     const doc = getHappySpec();
     renameKey(doc["/resource"], "/{resourceId}", "/{ID}");
     const result = await validateFile(renderSpecAsFile(doc), PROFILE);
