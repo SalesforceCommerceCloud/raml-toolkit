6b21d4091f07eb2b7b4411b1d4bbf41544959de8
Brian Redmond <bredmond@salesforce.com>
2020-05-29 16:29:42 -0400

    Clean up parser test

--- test/parser.test.ts
+++ test/parser.test.ts
@@ -5,188 +5,154 @@
  * For full license text, see the LICENSE file in the repo root or https://opensource.org/licenses/BSD-3-Clause
  */
 "use strict";
+
+import path from "path";
+
+import { model } from "amf-client-js";
+import { expect, default as chai } from "chai";
+import chaiAsPromised from "chai-as-promised";
+import _ from "lodash";
+
 import {
-  parseRamlFile,
   getAllDataTypes,
   getApiName,
+  parseRamlFile,
   resolveApiModel
 } from "../src/parser";
 
-import {
-  WebApiBaseUnitWithDeclaresModel,
-  WebApiParser,
-  model,
-  webapi,
-  WebApiBaseUnitWithEncodesModel
-} from "webapi-parser";
+const validRamlFile = path.join(__dirname, "site.raml");
+const invalidRamlFile = path.join(__dirname, "search-invalid.raml");
 
-import { expect } from "chai";
-import path from "path";
-import _ from "lodash";
+before(() => {
+  chai.use(chaiAsPromised);
+});
 
 describe("Test RAML file", () => {
-  it("Test invalid RAML file", () => {
-    const ramlFile = path.join(__dirname, "/raml/invalid/search-invalid.raml");
-    return expect(parseRamlFile(ramlFile)).to.be.empty;
-  });
+  it("Test invalid RAML file", () =>
+    expect(parseRamlFile(invalidRamlFile)).to.eventually.be.rejected);
 
-  it("Test valid RAML file", () => {
-    const ramlFile = path.join(__dirname, "site.raml");
-    return parseRamlFile(ramlFile)
-      .then(s => {
-        expect(s).to.not.equal(null);
-      })
-      .catch(e => {
-        expect.fail(e.message, "Valid RAML file parsing should not fail");
-      });
-  });
+  it("Test valid RAML file", () =>
+    expect(parseRamlFile(validRamlFile)).to.eventually.not.be.empty);
 });
 
 describe("Get Data types", () => {
-  it("Test valid RAML file", () => {
-    const ramlFile = path.join(__dirname, "site.raml");
-    return parseRamlFile(ramlFile).then(s => {
-      const res = getAllDataTypes(s as WebApiBaseUnitWithDeclaresModel);
-      expect(_.map(res, res => res.name.value())).to.be.deep.equal([
-        "product_search_result",
-        "ClassA",
-        "customer_product_list_item",
-        "query",
-        "ClassB",
-        "search_request",
-        "password_change_request",
-        "sort",
-        "result_page"
-      ]);
-    });
+  it("Test valid RAML file", async () => {
+    const baseUnit = await parseRamlFile(validRamlFile);
+    const dataTypes = getAllDataTypes(
+      baseUnit as model.document.BaseUnitWithDeclaresModel
+    );
+    const dataTypeNames = dataTypes.map(entry => entry.name.value());
+    return expect(dataTypeNames).to.deep.equal([
+      "product_search_result",
+      "ClassA",
+      "customer_product_list_item",
+      "query",
+      "ClassB",
+      "search_request",
+      "password_change_request",
+      "sort",
+      "result_page"
+    ]);
   });
 
-  it("Test valid RAML file with references", () => {
-    const ramlFile = path.join(__dirname, "site.raml");
-    parseRamlFile(ramlFile)
-      .then(refModel => {
-        return parseRamlFile(ramlFile).then(mainModel => {
-          mainModel.withReferences([refModel]);
-          return mainModel;
-        });
-      })
-      .then(s => {
-        const res = getAllDataTypes(s as WebApiBaseUnitWithDeclaresModel);
-        expect(_.map(res, res => res.name.value())).to.be.deep.equal([
-          "product_search_result",
-          "ClassA",
-          "customer_product_list_item",
-          "query",
-          "ClassB",
-          "search_request",
-          "password_change_request",
-          "sort",
-          "result_page"
-        ]);
-      });
+  it("Test valid RAML file with references", async () => {
+    const refModel = await parseRamlFile(validRamlFile);
+    const mainModel = await parseRamlFile(validRamlFile);
+    mainModel.withReferences([refModel]);
+    const dataTypes = getAllDataTypes(
+      mainModel as model.document.BaseUnitWithDeclaresModel
+    );
+    const dataTypeNames = dataTypes.map(entry => entry.name.value());
+    return expect(dataTypeNames).to.deep.equal([
+      "product_search_result",
+      "ClassA",
+      "customer_product_list_item",
+      "query",
+      "ClassB",
+      "search_request",
+      "password_change_request",
+      "sort",
+      "result_page"
+    ]);
   });
 });
 
 describe("Test that API Name is returned in lower camelCase", () => {
-  const domain = model.domain;
-  before(() => {
-    WebApiParser.init();
-  });
-
   const expectedApiName = "shopperCustomers";
-  it("Test with space in the name", () => {
-    const api = new domain.WebApi();
-    api.withName("Shopper Customers");
-    const model = new webapi.WebApiExternalFragment().withEncodes(api);
-    return expect(getApiName(model)).to.equal(expectedApiName);
-  });
-  it("Test with - in the name", () => {
-    const api = new domain.WebApi();
-    api.withName("Shopper-Customers");
-    const model = new webapi.WebApiExternalFragment().withEncodes(api);
-    return expect(getApiName(model)).to.equal(expectedApiName);
-  });
-  it("Test with _ in the name", () => {
-    const api = new domain.WebApi();
-    api.withName("Shopper-Customers");
-    const model = new webapi.WebApiExternalFragment().withEncodes(api);
-    return expect(getApiName(model)).to.equal(expectedApiName);
-  });
-  it("Test with . in the name", () => {
-    const api = new domain.WebApi();
-    api.withName("shopper.customers");
-    const model = new webapi.WebApiExternalFragment().withEncodes(api);
-    return expect(getApiName(model)).to.equal(expectedApiName);
-  });
-  it("Test with all lowercase name", () => {
-    const api = new domain.WebApi();
-    api.withName("shopper customers");
-    const model = new webapi.WebApiExternalFragment().withEncodes(api);
-    return expect(getApiName(model)).to.equal(expectedApiName);
-  });
-  it("Test with camelCase name", () => {
-    const api = new domain.WebApi();
-    api.withName("shopperCustomers");
-    const model = new webapi.WebApiExternalFragment().withEncodes(api);
-    return expect(getApiName(model)).to.equal(expectedApiName);
-  });
-  it("Test with null API name", () => {
-    const api = new domain.WebApi();
-    api.withName(null);
-    const model = new webapi.WebApiExternalFragment().withEncodes(api);
-    return expect(() => getApiName(model)).to.throw(
+
+  const testGetApiName = (name: string) => {
+    const api: model.domain.WebApi = new model.domain.WebApi();
+    api.withName(name);
+    const doc: model.document.Document = new model.document.Document();
+    doc.withEncodes(api);
+    return getApiName(doc);
+  };
+
+  it("returns lowerCamelCase with space in the name", () =>
+    expect(testGetApiName("Shopper Customers")).to.equal(expectedApiName));
+
+  it("returns lowerCamelCase with - in the name", () =>
+    expect(testGetApiName("Shopper-Customers")).to.equal(expectedApiName));
+
+  it("returns lowerCamelCase with _ in the name", () =>
+    expect(testGetApiName("Shopper_Customers")).to.equal(expectedApiName));
+
+  it("returns lowerCamelCase with . in the name", () =>
+    expect(testGetApiName("shopper.customers")).to.equal(expectedApiName));
+
+  it("returns lowerCamelCase with all lowercase name", () =>
+    expect(testGetApiName("shopper customers")).to.equal(expectedApiName));
+
+  it("returns lowerCamelCase with camelCase name", () =>
+    expect(testGetApiName("shopperCustomers")).to.equal(expectedApiName));
+
+  it("throws with null API name", () =>
+    expect(() => testGetApiName(null)).to.throw(
       "Invalid name provided to normalize"
-    );
-  });
-  it("Test with undefined API name", () => {
-    const api = new domain.WebApi();
-    api.withName(undefined);
-    const model = new webapi.WebApiExternalFragment().withEncodes(api);
-    return expect(() => getApiName(model)).to.throw(
+    ));
+
+  it("throws with undefined API name", () =>
+    expect(() => testGetApiName(undefined)).to.throw(
       "Invalid name provided to normalize"
-    );
-  });
-  it("Test with empty API name", () => {
-    const api = new domain.WebApi();
-    api.withName("");
-    const model = new webapi.WebApiExternalFragment().withEncodes(api);
-    return expect(() => getApiName(model)).to.throw(
+    ));
+
+  it("throws with empty API name", () =>
+    expect(() => testGetApiName("")).to.throw(
       "Invalid name provided to normalize"
-    );
-  });
+    ));
 });
 
 describe("Test resolving API model", () => {
-  it("Test with null model", () => {
-    return expect(() => resolveApiModel(null, "editing")).to.throw(
+  it("throws with null model", () =>
+    expect(() => resolveApiModel(null, "editing")).to.throw(
       "Invalid API model provided to resolve"
-    );
-  });
-  it("Test with undefined model", () => {
-    return expect(() => resolveApiModel(undefined, "editing")).to.throw(
+    ));
+
+  it("throws with undefined model", () =>
+    expect(() => resolveApiModel(undefined, "editing")).to.throw(
       "Invalid API model provided to resolve"
-    );
-  });
-  it("Test with null resolution pipeline", () => {
-    const apiModel = new webapi.WebApiExternalFragment();
+    ));
+
+  it("throws with null resolution pipeline", () => {
+    const apiModel: model.document.Document = new model.document.Document();
     return expect(() => resolveApiModel(apiModel, null)).to.throw(
       "Invalid resolution pipeline provided to resolve"
     );
   });
-  it("Test with undefined resolution pipeline", () => {
-    const apiModel = new webapi.WebApiExternalFragment();
+
+  it("throws with undefined resolution pipeline", () => {
+    const apiModel: model.document.Document = new model.document.Document();
     return expect(() => resolveApiModel(apiModel, undefined)).to.throw(
       "Invalid resolution pipeline provided to resolve"
     );
   });
-  it("Test with valid model and resolution pipeline", () => {
-    const ramlFile = path.join(__dirname, "site.raml");
-    return parseRamlFile(ramlFile)
-      .then(s => {
-        return resolveApiModel(s as WebApiBaseUnitWithEncodesModel, "editing");
-      })
-      .then(resolvedModel => {
-        expect(resolvedModel).to.exist;
-      });
+
+  it("returns model with valid model and resolution pipeline", async () => {
+    const testModel = await parseRamlFile(validRamlFile);
+    const resolved = resolveApiModel(
+      testModel as model.document.BaseUnitWithEncodesModel,
+      "editing"
+    );
+    return expect(resolved).to.not.be.null;
   });
 });
