2aa3c474a60d8e0ab94fd369e85ac93e51f97048
Will Harney <wharney@salesforce.com>
2020-06-22 17:05:46 -0400

    Use flags to make command functional.

--- src/download/command.ts
+++ src/download/command.ts
@@ -6,8 +6,7 @@
  */
 import fs from "fs-extra";
 import path from "path";
-import { Command } from "@oclif/command";
-import { ramlToolLogger } from "../common/logger";
+import { Command, flags } from "@oclif/command";
 import { getBearer } from "./bearerToken";
 import { extractFiles } from "./exchangeDirectoryParser";
 import {
@@ -23,27 +22,22 @@ import {
 } from "./exchangeTools";
 import { RestApi } from "./exchangeTypes";
 
-let config;
 /**
- * Gets information about all the apis from exchange that match config.search,
- * for the version deployed in the config.exchangeDeploymentRegex environment.
- * If it fails to get information about the deployed version of an api, it
- * removes all the version specific information from `config.apiConfigFile`.
+ * Gets information about all the APIs from exchange that match the given search
+ * string for the version deployed in the given environment.
+ * If it fails to get information about the deployed version of an API, it
+ * removes all the version specific information from the returned object.
  *
  * @returns Information about the APIs found.
  */
-async function search(): Promise<RestApi[]> {
+async function search(search: string, deployment: RegExp): Promise<RestApi[]> {
   const token = await getBearer(
     process.env.ANYPOINT_USERNAME,
     process.env.ANYPOINT_PASSWORD
   );
-  const apis = await searchExchange(token, config.exchangeSearch);
+  const apis = await searchExchange(token, search);
   const promises = apis.map(async api => {
-    const version = await getVersionByDeployment(
-      token,
-      api,
-      config.exchangeDeploymentRegex
-    );
+    const version = await getVersionByDeployment(token, api, deployment);
     return version
       ? getSpecificApi(token, api.groupId, api.assetId, version)
       : removeVersionSpecificInformation(api);
@@ -52,22 +46,63 @@ async function search(): Promise<RestApi[]> {
 }
 
 export default class DownloadCommand extends Command {
-  static description = ``;
-  static flags = {};
-  static args = [];
+  static description =
+    "Download API specification files from Anypoint Exchange";
+  static flags = {
+    help: flags.help({
+      char: "h"
+    }),
+    search: flags.string({
+      char: "s",
+      description: "Search query to filter results from Exchange",
+      env: "EXCHANGE_SEARCH",
+      default: 'category:"CC Visibility" = "External"'
+    }),
+    deployment: flags.string({
+      char: "D",
+      description: "Deployment status to filter results from Exchange",
+      env: "EXCHANGE_DEPLOYMENT",
+      default: "production"
+    }),
+    "deployment-regex-flags": flags.string({
+      description: "RegExp flags to specify for advanced deployment matching",
+      default: "i",
+      dependsOn: ["deployment"]
+    }),
+    dest: flags.string({
+      char: "d",
+      description: "Directory to download APIs into",
+      env: "EXCHANGE_DOWNLOAD_DEST",
+      default: "apis"
+    }),
+    family: flags.string({
+      char: "f",
+      description: "The category to use to group APIs together",
+      env: "API_FAMILY",
+      default: "CC API Family"
+    }),
+    "config-file": flags.string({
+      char: "c",
+      description: "",
+      env: "API_CONFIG_FILE",
+      default: "api-config.json"
+    })
+  };
   async run(): Promise<void> {
-    const apis = await search();
-    const folder = await downloadRestApis(apis, config.inputDir);
-    ramlToolLogger.info(`Setting config.inputDir to '${folder}'`);
-    config.inputDir = folder;
-    await extractFiles(folder);
+    const { flags } = this.parse(DownloadCommand);
+    const apis = await search(
+      flags.search,
+      new RegExp(flags.deployment, flags["deployment-regex-flags"])
+    );
+    await downloadRestApis(apis, flags.dest);
+    await extractFiles(flags.dest);
     const apiFamilyGroups = groupByCategory(
       removeRamlLinks(apis),
-      config.apiFamily
+      flags.family
     );
-    await fs.ensureDir(config.inputDir);
+    await fs.ensureDir(flags.dest);
     await fs.writeJson(
-      path.join(config.inputDir, config.apiConfigFile),
+      path.join(flags.dest, flags["config-file"]),
       apiFamilyGroups
     );
   }
