c7e6429090c8729eae7961cf9c0f8329560bf5d7
Brian Redmond <bredmond@salesforce.com>
2020-07-08 13:29:08 -0400

    Add index to generate

--- src/index.ts
+++ src/index.ts
@@ -38,9 +38,7 @@ export {
   resolveApiModel
 } from "./common/parser";
 
-export { default as Api } from "./generate/api";
-export { default as ApiGroup } from "./generate/apiGroup";
-export { default as ApiCollection } from "./generate/apiCollection";
+export { Api, ApiGroup, ApiCollection } from "./generate";
 
 export { findApiChanges, diffRaml, NodeDiff, RamlDiff } from "./diff";
 export { Template } from "./common/template";
