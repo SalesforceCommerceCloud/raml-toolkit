ab1bc82e1902d9d923e059c73bb1e5ed93f0c313
Gaurav Khunger <gkhunger@salesforce.com>
2020-07-17 14:06:19 -0400

    Rename handlebars helpers and its utils

--- src/generate/handlebarsAmfHelpers.test.ts
+++ src/generate/handlebarsAmfHelpers.test.ts
@@ -5,7 +5,7 @@
  * For full license text, see the LICENSE file in the repo root or https://opensource.org/licenses/BSD-3-Clause
  */
 import {
-  getBaseUri,
+  getBaseUriFromDocument,
   getProperties,
   isAdditionalPropertiesAllowed,
   isOptionalProperty,
@@ -22,20 +22,22 @@ describe("HandlebarsAmfHelpers", () => {
     return AMF.init();
   });
 
-  describe("getBaseUri", () => {
+  describe("getBaseUriFromDocument", () => {
     it("returns an empty string for null input", () => {
-      expect(getBaseUri(null)).to.equal("");
+      expect(getBaseUriFromDocument(null)).to.equal("");
     });
 
     it("returns an empty string for empty model", () => {
-      expect(getBaseUri(new model.document.Document())).to.equal("");
+      expect(getBaseUriFromDocument(new model.document.Document())).to.equal(
+        ""
+      );
     });
 
     it("returns correct base uri", async () => {
       const api: model.domain.WebApi = new model.domain.WebApi();
       api.withServer("test-url-value");
       const testModel = new model.document.Document().withEncodes(api);
-      expect(getBaseUri(testModel)).to.equal("test-url-value");
+      expect(getBaseUriFromDocument(testModel)).to.equal("test-url-value");
     });
   });
 
