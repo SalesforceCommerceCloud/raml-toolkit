e11c45f07d06bc989d58110c4cb352e2a08a072c
Adam Raya Navarro <arayanavarro@salesforce.com>
2024-05-31 10:15:22 -0700

    WIP debug CI update downloadCommand changes

--- src/download/downloadCommand.ts
+++ src/download/downloadCommand.ts
@@ -47,7 +47,7 @@ export class DownloadCommand extends Command {
     }
     const { flags } = this.parse(DownloadCommand);
 
-    if (flags.deployment !== ".") {
+    if (flags.deployment !== "." || flags["deployment-regex-flags"] !== ".") {
       this.warn(
         "The options 'deployment' and 'deployment-regex-flags' are deprecated and currently non-functional due to changes in the RAML spec. These options will be removed in the next major version."
       );
